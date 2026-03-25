5823a16905821c1132cd3801d6d652ff3c64ca64
Anuj Gupta <114681833+AnujGupta1606@users.noreply.github.com>
2026-03-25 12:36:42 +0530

    DonationAdapterUpdated

--- app/src/main/java/com/example/androidproject/DonationAdapter.kt
+++ app/src/main/java/com/example/androidproject/DonationAdapter.kt
@@ -1,4 +1,40 @@
-package com.example.androidproject
+package com.example.feedforward
 
-class DonationAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class DonationAdapter(
+    private val donationList: List<Donation>,
+    private val onItemClick: (String) -> Unit
+) : RecyclerView.Adapter<DonationAdapter.DonationViewHolder>() {
+
+    class DonationViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val foodName: TextView = view.findViewById(R.id.tvItemFoodName)
+        val quantity: TextView = view.findViewById(R.id.tvItemQuantity)
+        val status: TextView = view.findViewById(R.id.tvItemStatus)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DonationViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_donation, parent, false)
+        return DonationViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DonationViewHolder, position: Int) {
+        val donation = donationList[position]
+
+ 
+        holder.foodName.text = donation.foodName
+        holder.quantity.text = "Qty: ${donation.quantity}"
+        holder.status.text = "Status: ${donation.status}"
+
+        holder.itemView.setOnClickListener {
+            
+            onItemClick(donation.donationId)
+        }
+    }
+
+    override fun getItemCount() = donationList.size
 }
